6431b5fedecf73fc18e1afbb82936df9aad83cda
TolaTien <tien0bip009@gmail.com>
2026-01-25 23:25:41 +0700

    init prisma

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,26 +1,59 @@
-import express, {Application} from 'express';
-import { connectDB } from './config/connectDB';
+import express, { Application, Request, Response } from "express";
+import {prisma} from "./config/prisma";
+import { v4 as uuid } from 'uuid';
+export class App {
+  private app: Application;
 
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
 
-export class App{
-    private app: Application;
-    constructor(){
-        this.app = express();
-        this.routes()
-    }
+  private middlewares(): void {
+    this.app.use(express.json());
+  }
 
-    private routes() {
-        this.app.get('/', (req, res) => {
-            res.send('API is running');
+  private routes(): void {
+    // Health check
+    this.app.get("/", (req: Request, res: Response) => {
+      res.send("API is running");
+    });
+
+    // TEST CREATE USER
+    this.app.post("/users", async (req: Request, res: Response) => {
+      try {
+        const { username, email, password } = req.body;
+
+        const user = await prisma.users.create({
+          data: {
+            user_id: uuid(),
+            username,
+            email,
+            password,
+          },
         });
-    }
-    public async start(): Promise<void>{
-        await connectDB()
-        const PORT  = 3000;
-        this.app.listen(PORT, () => {
-            console.log("Server listening on port: " + PORT);
-        })
-    }
 
+        res.json(user);
+      } catch (error: any) {
+        res.status(500).json({
+          message: "Create user failed",
+          error: error.message,
+        });
+      }
+    });
 
+    // TEST GET USERS
+    this.app.get("/users", async (req: Request, res: Response) => {
+      const users = await prisma.users.findMany();
+      res.json(users);
+    });
+  }
+
+  public async start(): Promise<void> {
+    const PORT = 3000;
+    this.app.listen(PORT, () => {
+      console.log("🚀 Server listening on port: " + PORT);
+    });
+  }
 }
---
+++ Backend/src/controllers/user.controller.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response } from "express";
+import {prisma} from '../config/prisma'
+import { v4 as uuid } from 'uuid';
+
+
+export class User{
+
+    
+
+
+
+
+}
